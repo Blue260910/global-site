7a69329740bad25e56f841bcf9d3e5e970085923
Blue260910 <rm99667@fiap.com.br>
2023-11-14 23:40:18 -0300

    Co-authored-by: Julia Azevedo Lins <JuliaAzevedoLins@users.noreply.github.com> Co-authored-by: Luis Garrido  <lgustavobarre351@users.noreply.github.com>

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,28 @@
-import styled from "styled-components";
+import Form from 'react-bootstrap/Form';
 
-function Home() {
+function FormGroupExample() {
+  fetch('https://hapbox-f9157-default-rtdb.firebaseio.com/.json')
+  .then(response => response.json())
+  .then(data => 
+    console.log(data)
+
+    )
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
   return (
-    <div className="Home">
-      <h1> Filler da Home</h1>
-    </div>
+    <Form>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+    </Form>
   );
 }
 
-export default Home;
+export default FormGroupExample;
